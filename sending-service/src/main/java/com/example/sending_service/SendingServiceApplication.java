ebb727f118dd76d26d744f3b552ac67bc0726d51
hamza diniawi <60050013+hamzadiniawi@users.noreply.github.com>
2024-12-31 16:10:39 +0100

    #1 first commit

---
+++ sending-service/src/main/java/com/example/sending_service/SendingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.sending_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendingServiceApplication.class, args);
+	}
+
+}
